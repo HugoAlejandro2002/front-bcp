09eb07c51b96394630d8cc0c3c2a05e61b6f7e20
HugoAlejandro2002 <apazahuaychohugoalejandro@gmail.com>
2024-06-30 04:51:21 -0400

    feat: improved brand selection

--- src/components/ProductForm.jsx
+++ src/components/ProductForm.jsx
@@ -4,9 +4,19 @@ const ProductForm = ({ onSubmit }) => {
   const [providerName, setProviderName] = useState('');
   const [modelIdentifier, setModelIdentifier] = useState('');
 
+  const manufacturers = [
+    { name: 'Frigidaire', models: ['GHWQ083WC1',] },
+    { name: 'General Electric', models: ['GFW550SPRRS',] }
+  ];
+
   const handleSubmit = (e) => {
     e.preventDefault();
     onSubmit({ providerName, modelIdentifier });
+  };
+
+  const handleProviderChange = (e) => {
+    setProviderName(e.target.value);
+    setModelIdentifier('');
   };
 
   return (
@@ -20,27 +30,40 @@ const ProductForm = ({ onSubmit }) => {
             <label className="block text-text-primary font-bold mb-2" htmlFor="providerName">
               Nombre del fabricante
             </label>
-            <input
+            <select
               className="w-full px-3 py-2 border rounded-lg"
-              type="text"
               id="providerName"
               value={providerName}
-              onChange={(e) => setProviderName(e.target.value)}
+              onChange={handleProviderChange}
               required
-            />
+            >
+              <option value="">Seleccione un fabricante</option>
+              {manufacturers.map((manufacturer) => (
+                <option key={manufacturer.name} value={manufacturer.name}>
+                  {manufacturer.name}
+                </option>
+              ))}
+            </select>
           </div>
           <div className="mb-4">
             <label className="block text-text-primary font-bold mb-2" htmlFor="modelIdentifier">
               Identificador de modelo
             </label>
-            <input
+            <select
               className="w-full px-3 py-2 border rounded-lg"
-              type="text"
               id="modelIdentifier"
               value={modelIdentifier}
               onChange={(e) => setModelIdentifier(e.target.value)}
               required
-            />
+              disabled={!providerName}
+            >
+              <option value="">Seleccione un modelo</option>
+              {providerName && manufacturers.find(manufacturer => manufacturer.name === providerName).models.map((model) => (
+                <option key={model} value={model}>
+                  {model}
+                </option>
+              ))}
+            </select>
           </div>
           <div className="flex flex-col md:flex-row justify-between items-center">
             <button
