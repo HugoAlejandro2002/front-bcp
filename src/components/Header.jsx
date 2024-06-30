4f4f8e2bef4811ca20c7c5f5907e4c666df4b6aa
HugoAlejandro2002 <apazahuaychohugoalejandro@gmail.com>
2024-06-30 08:54:12 -0400

    feat: changed to oficial icons

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,17 +1,19 @@
 import { Link } from 'react-router-dom';
 import bcp_logo from '../assets/bcp.png';
+import bcverde_logo from '../assets/logo_bcverde.jpeg';
+
 
 const Header = () => {
   return (
     <header className="bg-primary text-white p-4">
       <div className="container mx-auto flex justify-between items-end">
-        <div className="flex items-center">
+        <div className="flex items-center" style={{ maxHeight: '2rem' }} >
           <Link to="/" className="logo" title="INICIO">
-            <img src={bcp_logo} alt="LABEL 2020" className="w-20 h-20 mr-2 object-contain" style={{ maxHeight: '2rem' }} />
+            <img src={bcp_logo} alt="LABEL 2020" className="w-32 h-32 mr-2 object-contain"/>
           </Link>
         </div>
         <div className="slogan text-xl">
-          <span className="h2">BCVERDE</span>
+          <img src={bcverde_logo} alt="LABEL 2020" className="mr-2 object-contain" style={{ maxHeight: '2rem' }}/>
         </div>
       </div>
     </header>
